0ea709be8a32089f8e1b3f1503356bf8c2e3f585
KaxiaLee <kaxia901012@gmail.com>
2024-01-01 21:15:46 +0900

    upload

---
+++ day1.java
@@ -0,0 +1,22 @@
+//인간=자연어(natural language)를 쓰고 표현에 무한성을 띈다.
+//기계=기계어(machine language)를 쓰고 자연어에 비해 매우 형식적이고 제한적 표현만 가능하며, 데이터의 유한성이 있다.
+//순차적으로 일 처리 하는걸 프로그램이라 한다.
+//컴파일러(Compiler) = 번역기
+//고급언어(high level language) = 사람이 이해하기 쉬운 프로그래밍 언어를 말한다.
+//개발자가 대표적으로 기본으로 알고 있는 언어 5가지 = HTML, JAVA, JAVA SCRIPT, PYTHON, SQL
+/*  programing language 베이스를 잘 깔아야 위 5가지 언어도 비교적 쉽게 습득 가능 기초가 중요 */
+//JRE(JAVA RUNTIME ENVIROMENT) = 자바 개발 환경의 약자
+//JDK(JAVA DEVELOPMENT KIT) = 자바 개발 환경 키트
+//JKD의 구성 요소 = 1.JAVA COMPILER - 원시코드(Source Code)를 가상머신이 이해하는 코드(Byte code)로 변환해주는 s/w(소프트웨어)
+//                 2.LOADER - Byte code를 가상머신에게 전달하는 s/w
+//                 3.JVM(Java Virtual Machine) - 자바 프로그램이 어느 기기, 어느 운영체제든 상관없이 실행가능하도록 이식성을 확보 및 보장한다.
+//토큰(Token) = 분리할 수 없는 한 단어
+//예약어 = 식별자 ex) void, public, static ...
+//IDE(Integrated Development Environment) = 통합 개발 환경이란 프로그래머가 소프트웨어 코드를 효율적으로 개발하도록 돕는 소프트웨어 애플리케이션
+//IDE가 중요한 이유는 ? 1.코드 편집 자동화 2.구문 강조 표시 3.지능형 코드 완성 4.디버깅 등등
+
+public class day1{
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
